56d83731e1bba6d88c479507cce6d4907640dd02
stephp23 <stephp23@gmail.com>
2021-04-30 02:19:33 -0400

    removal of additonal childSchema

--- models/employee.js
+++ models/employee.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const childScheme = new Schema({ address: {
-  street: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  zip: { type: String, required: true }
-  }})
 
 const Employee = new Schema(
   {
-    _id: ObjectId { type: Schema.Types.ObjectId, ref: '_id'},
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     email: { type: String, required: true },
